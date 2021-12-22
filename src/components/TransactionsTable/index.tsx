6665dbb13a5716932ff505193b3a2c9c117cec24
Pablo Vicente <pablo.vicente@outlook.com.br>
2021-12-22 19:07:44 -0300

    mirage and axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+    useEffect(() => {
+        api.get('transactions')
+            .then(res => console.log(res))
+    }, [])
+
     return (
         <Container>
             <table>
                 <thead>
-                    <th>Título</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr>
