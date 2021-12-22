8f4633ffc85700b5542b2361dec8b1d896e2427e
Pablo Vicente <pablo.vicente@outlook.com.br>
2021-12-21 21:26:53 -0300

    TransactionsTable

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,40 @@
+import { Container } from "./styles";
+import incomeSvg from '../../assets/income.svg'
+import outcomeSvg from '../../assets/outcome.svg'
+import totalSvg from '../../assets/total.svg'
+
+export function Summary() {
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeSvg} alt="Entradas" />
+                </header>
+                <strong>
+                    R$ 1.000,00
+                </strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeSvg} alt="Saídas" />
+                </header>
+                <strong>
+                    -R$ 500,00
+                </strong>
+            </div>
+
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalSvg} alt="Total" />
+                </header>
+                <strong>
+                    R$ 500,00
+                </strong>
+            </div>
+        </Container>
+    )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #f0f2f5;
         --red: #E52e40;
         --blue: #5429CC;
+        --green: #33cc95;
 
         --blue-light: #6933FF;
 
